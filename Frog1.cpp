aeae4adc8bf9a2e34fad40dea85befb0176f431c
sL0p3 <shlokjakhotia@gmail.com>
2022-07-01 06:35:45 +0530

    updated

--- Frog1.cpp
+++ Frog1.cpp
@@ -65,6 +65,21 @@ ll lcm(ll a, ll b) {return ((a * b) / (gcd(a, b)));}
 
 /*-------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
+const int N = 1e7;
+int h[N],dp[N];
+
+int func(int i){
+    if(i==0) return 0;
+    if(dp[i]!= -1) return dp[i];
+    int cost = INT_MAX;
+    // way 1 
+    cost = min(cost,func(i-1) + abs(h[i] - h[i-1]));    
+    // way 2
+    if(i>1)
+    cost = min(cost,func(i-2) + abs(h[i] - h[i-2])); 
+    return dp[i] = cost;   
+}
+
 int main() {
 
 #ifndef ONLINE_JUDGE
@@ -75,8 +90,13 @@ int main() {
 
     fastio();
 
-    
-
+    memset(dp , -1, sizeof(dp));
+    int n;
+    cin>>n;
+    for(int i=0 ;i<n;i++){
+        cin>>h[i];
+    }
+    cout<<func(n-1)<<endl;
     return 0;
 }
 
